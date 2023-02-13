cb29c88a5bb138d551b7927e39a551499c19c625
JaiyeGesinde <jayegesinde@gmail.com>
2023-02-13 21:15:18 +0000

    cart context, shopping icon + cart dropdown

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -10,8 +10,6 @@ export const UserContext = createContext ({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
-
-    //signOutUser()
         
     useEffect (() => {
        const unsubscibe = onAuthStateChangedListener((user) => {
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { UserProvider } from './contexts/user.context';
+import { ProductProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import './index.scss'
 
 import {createRoot} from 'react-dom/client';
@@ -15,7 +17,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
